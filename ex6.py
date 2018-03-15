92f2425f2753b873546d7160146264eb90e215b7
Alpha Variable <vikasoffice81@gmail.com>
2018-03-15 19:12:10 +0800

    minor adjustments

---
+++ ex6.py
@@ -0,0 +1,11 @@
+x = "There are %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+
+y = "Those of us who know %s and those of us who %s." % (binary, do_not)
+print "I said %r " % x
+print "I also said %s " % y
+w = "This is the left side of..."
+e = "a string with the right side"
+
+print w + e
