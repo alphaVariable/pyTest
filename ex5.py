ae401bddd0c60f1b1fb4b896b672f643bd57e847
Alpha Variable <vikasoffice81@gmail.com>
2018-03-15 17:45:56 +0800

    moving last time

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Vitalik Buterin"
+my_age = 33
+my_height = 170
+my_weight = 60
+my_eyes = "brown"
+my_teeth = "brownish"
+my_hair = "white"
+
+print "Let's talk about my %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d kilo heavy." % my_weight
+print "That's not too fucking heavy"
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s, depending on the colour of the coffee" % my_teeth
+
+#fucking octothorpe sign. Anyways, this line is supposed to be a bit heavy, try not to fuck it up, mate
+print "If I add %d, %d and %d, I get %d" % (my_age, my_weight, my_height, my_age + my_weight + my_height)
