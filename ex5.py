68d1ceb741e38e22e444a4b363bd7afa202b2495
Alpha Variable <vikasoffice81@gmail.com>
2018-03-15 18:49:53 +0800

    minor adjustments

--- ex5.py
+++ ex5.py
@@ -1,14 +1,18 @@
 name = "Vitalik Buterin"
 age = 33
-height = 170
-weight = 60
+height = 170 #cm
+height_in_inches = height * 0.393701
+weight = 60 #kg
+weight_in_stone = weight * 0.157473044
+weight_in_pound = weight * 2.20462
 eyes = "brown"
 teeth = "brownish"
 hair = "white"
 
 print "Let's talk about my %s." % name
-print "He's %d inches tall." % height
-print "He's %d kilo heavy." % weight
+print "He's %d cm tall." % height
+print "He's %d inches tall." % height_in_inches
+print "He's %d kilo heavy. That's %r pounds or %s stones. Imagine being that heavy, lol." % (weight, weight_in_pound, weight_in_stone)
 print "That's not too fucking heavy"
 print "He's got %s eyes and %s hair" % (eyes, hair)
 print "His teeth are usually %s, depending on the colour of the coffee" % teeth
