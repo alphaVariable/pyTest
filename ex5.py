fd540fd3e346a01fd22cf508a752979b6d54d0f5
Alpha Variable <vikasoffice81@gmail.com>
2018-03-15 18:43:04 +0800

    minor adjustments

--- ex5.py
+++ ex5.py
@@ -1,17 +1,18 @@
-my_name = "Vitalik Buterin"
-my_age = 33
-my_height = 170
-my_weight = 60
-my_eyes = "brown"
-my_teeth = "brownish"
-my_hair = "white"
+name = "Vitalik Buterin"
+age = 33
+height = 170
+weight = 60
+eyes = "brown"
+teeth = "brownish"
+hair = "white"
 
-print "Let's talk about my %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d kilo heavy." % my_weight
+print "Let's talk about my %s." % name
+print "He's %d inches tall." % height
+print "He's %d kilo heavy." % weight
 print "That's not too fucking heavy"
-print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
-print "His teeth are usually %s, depending on the colour of the coffee" % my_teeth
+print "He's got %s eyes and %s hair" % (eyes, hair)
+print "His teeth are usually %s, depending on the colour of the coffee" % teeth
 
 #fucking octothorpe sign. Anyways, this line is supposed to be a bit heavy, try not to fuck it up, mate
-print "If I add %d, %d and %d, I get %d" % (my_age, my_weight, my_height, my_age + my_weight + my_height)
+print "If I add %d, %d and %d, I get %d" % (age, weight, height, age + weight + height)
+print "If I multiply %r, %s, %s and %r, I get %r" % (eyes, age, weight, height, age * weight * height)
