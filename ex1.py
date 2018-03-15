d9f956e2964918ba5a7274f303f8335acc093616
Alpha Variable <vikasoffice81@gmail.com>
2018-03-15 17:42:00 +0800

    moved around

---
+++ ex1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Something in the second line"
+print "this is fun stuff"
+print 'this line is with just a single quote'
+print "I try to esca\"pe the double quote"
+print "a new fucking line"
---
+++ ex3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+print "I will now count my chicken"
+
+# adds the amount of chicken
+print "Hens", 4 + 3 * 3
+print "Rooster", 4 * 3 + 3
+
+print "I will now count my eggs"
+
+print 4 * 4 + 33 - 3
+print 4 * 4 < 4 + 4
+
+# Just wanted to check the asterisk character
+print "4 * 4", 4*4
+print "4-6", 4-6
+
+print 4/6
+print 4.0/6.0
