b7f5ee58289ae41239cee46981ed58172ce77ed5
Alpha Variable <vikasoffice81@gmail.com>
2018-03-16 06:15:15 +0800

    minor adjustments

--- ex6.py
+++ ex6.py
@@ -5,6 +5,12 @@ do_not = "don't"
 y = "Those of us who know %s and those of us who %s." % (binary, do_not)
 print "I said %r " % x
 print "I also said %s " % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
 w = "This is the left side of..."
 e = "a string with the right side"
 
