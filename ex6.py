40b7a39e6c7aa2d0eeffa17d944037d436103cf2
Alpha Variable <vikasoffice81@gmail.com>
2018-03-16 10:29:51 +0800

    minor adjustments

--- ex6.py
+++ ex6.py
@@ -2,16 +2,20 @@ x = "There are %d types of people" % 10
 binary = "binary"
 do_not = "don't"
 
+# a string was put inside of string here
 y = "Those of us who know %s and those of us who %s." % (binary, do_not)
+# a string was put inside of a string here
 print "I said %r " % x
+# a string was also put inside of a string here
 print "I also said %s " % y
 
 hilarious = False
+# This is not a string going into a string, it's a boolean
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
 w = "This is the left side of..."
 e = "a string with the right side"
-
+#Concateniation. Yay! :|
 print w + e
---
+++ ex7.py
@@ -0,0 +1,21 @@
+print "#" * 40 + " " * 10 + "Start of Mary's stupid tale" + " " * 8 + "#" * 40  # Eliminates repetitive typing.
+print "Mary had a little lamb."
+print "It's fleece was as white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "#" * 40 + " " * 10 + "End of Mary's stupid tale" + " " * 10 + "#" * 40  # Eliminates repetitive typing.
+
+end1 = "V"
+end2 = "e"
+end3 = "g"
+end4 = "a"
+end5 = "n"
+
+end6 = "B"
+end7 = "u"
+end8 = "r"
+end9 = "g"
+end10 = "e"
+end11 = "r"
+
+print end1 + end2 + end3 + end4 + end5
+print end6 + end7 + end8 + end9 + end10 + end11
