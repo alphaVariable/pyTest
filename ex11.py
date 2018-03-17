e4e6be9322c1e3fb0c8e8a2c84b60a3e8ec2b2b7
Alpha Variable <vikasoffice81@gmail.com>
2018-03-17 12:50:09 +0800

    let's improve raw_input()

--- ex11.py
+++ ex11.py
@@ -1,8 +1,8 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How heavy are you?",
-weight = raw_input()
+# print "How old are you?",
+age = raw_input('How old are you? ')
+# print "How tall are you?",
+height = raw_input('How tall are you? ')
+# print "How heavy are you?",
+weight = raw_input('How heavy are you? ')
 
 print "So you're %s years old %s cm tall and... jeesus fucking christ, %s kg heavy?" % (age, height, weight)
