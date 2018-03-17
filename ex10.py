050a5ecd86be33b9ea5468aa1e92405559dd34b3
Alpha Variable <vikasoffice81@gmail.com>
2018-03-17 20:17:29 +0800

    let's add all (well, all that I have found) escapes and special characters

--- ex10.py
+++ ex10.py
@@ -16,5 +16,23 @@ print fat_cow
 
 # while True:
 #     for i in ["/", "-", "|", "\\", "|"]:
-#         print "%s\r" % i,
-# I don't fucking understand this code just yet. Execute at your own peril
+#         print "%r\r" % i,
+# I don't fucking understand this code just yet. Execute at your own peril. In case you get stuck, use control + C to terminate script processing. 
+
+#Escapes in Python, and how to print the special characters
+print '%r -> %s' % ('\\','\\')
+print '%r -> %s' % ('\'','\'')
+print '%r -> %s' % ("\"","\"")
+print '%r -> %s' % ('\a','\a')
+print '%r -> %s' % ('\b','\b')
+print '%r -> %s' % ('\f','\f')
+print '%r -> %s' % ('\n','\n')
+s = u"\N{BLACK SPADE SUIT}"
+print '%r -> %s' % (s,s)
+print '%r -> %s' % ('\r','\r')
+print '%r -> %s' % ('\thorizontal tab','\thorizontal tab')
+print '%r -> %s' % (u'\u0908',u'\u0908') # 16 bit unicode characters are printed like this
+print '%r -> %s' % (u'\U000003bb',u'\U000003bb') # 32 bit unicode characters are printed like this
+print '%r -> %s' % ('\v','\v')
+print '%r -> %s' % ('\176','\176') #octal characters
+print '%r -> %s' % ('\x7a','\x7a') #hex characters
