40cd71d38bea03081a19acb450bcab877259afff
Alpha Variable <vikasoffice81@gmail.com>
2018-03-17 12:38:39 +0800

    py

--- ex10.py
+++ ex10.py
@@ -2,13 +2,13 @@
 tabby_cow = "\tI'm tabbed in." #\t
 persian_cow = "I'm split\non a line." #\n
 backslash_cow = "I'm \\ a \\ cow." #\\
-fat_cow = """
+fat_cow = '''
 Here's a fucking list of what cows want:
 \t*Love
 \t*Interaction
 \t*Not be killed\n\t*Grass
 \t  *Peace
-""" #Additional space on the last bullet point will get printed.    
+''' #Additional space on the last bullet point will get printed.    
 print tabby_cow
 print persian_cow
 print backslash_cow
