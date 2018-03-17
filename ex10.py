fe38402bd92da4d1e26fc7c89bd017aad9d5418b
Alpha Variable <vikasoffice81@gmail.com>
2018-03-17 09:45:16 +0800

    py

--- ex10.py
+++ ex10.py
@@ -12,4 +12,8 @@ Here's a fucking list of what cows want:
 print tabby_cow
 print persian_cow
 print backslash_cow
-print fat_cow 
+print fat_cow
+
+while True:
+    for i in ["/", "-", "|", "\\", "|"]:
+        print "%s\r" % i,
