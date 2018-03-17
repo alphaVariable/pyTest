a5dec410f573ddf54f2a1bffb7dd0e624bdec447
Alpha Variable <vikasoffice81@gmail.com>
2018-03-17 12:47:45 +0800

    let's try raw_input()

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How heavy are you?",
+weight = raw_input()
+
+print "So you're %s years old %s cm tall and... jeesus fucking christ, %s kg heavy?" % (age, height, weight)
