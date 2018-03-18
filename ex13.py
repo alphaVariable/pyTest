049127146710d4687b0addb4ab8dd682202688f9
Alpha Variable <vikasoffice81@gmail.com>
2018-03-18 17:20:10 +0800

    minor changes

--- ex13.py
+++ ex13.py
@@ -1,8 +1,14 @@
 from sys import argv
 
-script, first, second, third = argv
+# script, first, second, third = argv
 
-print "The script is called: ", script
-print "Your first variable is called:", first
-print "Your second variable is called:", second
-print "Your third variable is called:", third
+# print "The script is called: ", script
+# print "Your first variable is called:", first
+# print "Your second variable is called:", second
+# print "Your third variable is called:", third
+
+script, answer1, answer2 = argv
+
+print "Thefile name, aka 'script' is called:", script
+print "The capital of Australia is:", answer1
+print "The capital of Nigeria is:", answer2
