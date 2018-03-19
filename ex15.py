90f95cbe0040b5896bdcfa6ed822402d87152609
Alpha Variable <vikasoffice81@gmail.com>
2018-03-19 19:44:26 +0800

    minor changes

--- ex15.py
+++ ex15.py
@@ -1,9 +1,14 @@
+# Imports argv module
 from sys import argv
-
+# declares variables, filename to be opened
 script, filename = argv
 
+# opens the file entered by the user
 txt = open(filename)
-print "Here's your file %r:" % filename
+# prints the filename as confirmation to the user
+print "Here's your file \033[0;31m%r\033[0m:" % filename
+# Reads text (& prints to the terminal)
+print txt.read()
 
 print "type the filename again:"
 
