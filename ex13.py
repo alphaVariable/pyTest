a7eddbae32f11463b4e62db1276a373f9b289059
Alpha Variable <vikasoffice81@gmail.com>
2018-03-19 08:09:08 +0800

    minor adjustments

--- ex13.py
+++ ex13.py
@@ -13,5 +13,5 @@ print "Thefile name, aka 'script' is called:", script
 print "The capital of Australia is:", answer1
 print "The capital of Nigeria is:", answer2
 answer3 = raw_input("What's your name? ")
-answer4 = raw_input("How old are you? ")
-print "Congratulations %s, you may have answered the question correctly, I am not up to conditional statements just yet so enjoy it while you can" % (answer3, answer4)
+answer4 = int(raw_input("How old are you? "))
+print "Congratulations %d year old %s, you may have answered the question correctly, I am not up to conditional statements just yet so enjoy it while you can" % (answer4, answer3)
